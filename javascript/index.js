ea2a81db57c919678dc3ceae91863c99cb04a5ee
EILIDH SOUTHREN (1513195) <1513195@rgu.ac.uk>
2018-09-19 16:23:01 +0100

    tag search update

--- javascript/index.js
+++ javascript/index.js
@@ -23,19 +23,19 @@ function displayAllPosts() {
                     '<table class="postHeader"><tr><td><h2>' + childData['title'] + '</h2></td>' +
                     '<td class="postHeaderDate"><h3>' + childData['date'] + '</h3></td></tr></table>' ;
 
-            var blurb = childData['text'].slice(0,250);               
+            var blurb = childData['text'].slice(0,250);
             // Strip HTML tags so we don't get images in the posts list
-            var blurbStripped = blurb.replace(/<[^>]+>/g, ''); 
+            var blurbStripped = blurb.replace(/<[^>]+>/g, '');
             string_of_posts += blurbStripped + '....<strong> Continue</strong><br /><h5>';
             // Display all tags of a post
             for(var i = 0; i < childData['tags'].length; i++) {
-                string_of_posts += '<mark>&nbsp;' + childData['tags'][i] + '&nbsp;</mark>&nbsp;&nbsp;';
+                string_of_posts += '<mark class="tag">&nbsp;' + childData['tags'][i] + '&nbsp;</mark>&nbsp;&nbsp;';
             }
             string_of_posts += '</h5></div></div>';
         });
         // Map HTML to #main div content
         var list_of_posts = string_of_posts;
-        $('#main').html(list_of_posts);   
+        $('#main').html(list_of_posts);
     });
 }
 
@@ -63,10 +63,10 @@ function findPostFromParameter(para) {
                 return true;
             }
         });
-    });            
+    });
 }
 
-var para = getUrlParameter('post');    
+var para = getUrlParameter('post');
     if (para == "" ) {
         // If the URL has no parameter, display all posts
         displayAllPosts();
@@ -97,16 +97,16 @@ function displayPost(postNum) {
                 '<h5><strong>Tags </strong>';
                 // Display all tags
                 for(var i = 0; i < childData['tags'].length; i++) {
-                    post_string += '<mark>&nbsp;' + childData['tags'][i] + '&nbsp;</mark>&nbsp;&nbsp;';  
+                    post_string += '<mark>&nbsp;' + childData['tags'][i] + '&nbsp;</mark>&nbsp;&nbsp;';
                 }
                 post_string += '</h5>' + childData['text'] + '<br /><br />' +
                     '<h3><a href="index.html">< Return</a></h3></div></div>';
                 post_title = childData['title'];
-            }                  
+            }
         });
     });
     // add url parameter of post title
-    var para = getUrlParameter('post'); 
+    var para = getUrlParameter('post');
     if (para != post_title) {
         addOrUpdateUrlParam("post", post_title);
     }
@@ -117,16 +117,16 @@ function displayPost(postNum) {
 }
 
 function addOrUpdateUrlParam(name, value) {
-    // Add value parameter to url 
+    // Add value parameter to url
     var href = window.location.href;
     var pageUrl = window.location.href + "?post=" + value;
-    window.history.pushState('','',pageUrl);                
+    window.history.pushState('','',pageUrl);
 }
 
 function getPostAppend(num) {
     if(parseInt(num) % 2 == 0 || num == 1) {
-        return "";                     
-    } 
+        return "";
+    }
     else {
         return "2";
     }
--- javascript/tagsearch.js
+++ javascript/tagsearch.js
@@ -1,16 +1,4 @@
-//tags = new Set();
 
-// todo 
-
-/*
-create tags class to display them nicely
-
-add a 'display all' button which pops all the tags back to active
-
-make update filters button do the thing
-
-add explanation or little eye/eye hidden logo to clarify what the toggling does?
-*/
 
 function displayTags() {
     // display a list of tags used in posts
@@ -25,40 +13,39 @@ function displayTags() {
                 var temp = childData['tags'][i];
                 tags.add(temp);
             }
-     }); 
+     });
         var tagNum = 0;
         for (var it = tags.values(), val= null; val=it.next().value; ) {
-            console.log("tagnum: " + tagNum);
-            tags_string += '<mark class="tagNum' + tagNum + '" onclick="toggleTag(' + tagNum++ + ')">&nbsp;' + val + ' x &nbsp;</mark>&nbsp;&nbsp;';
+            tags_string += '<mark class="tagSearch" id="tagNum' + tagNum + '" onclick="toggleTag(' + tagNum++ + ')">&nbsp;' + val + ' &nbsp;</mark>&nbsp;&nbsp;';
          }
        $('#tags').html(tags_string);
+       tagList = tags;
      });
 }
 
 function showTaggedPosts() {
-    
+    console.log("updating posts...");
+    tags = getSelectedTagList();
+    console.log(tags);
     // todo: only display post if the tag filter is true
-    
-        var database = firebase.database();
+
+    var database = firebase.database();
     var string_of_posts = "";
-        var dataDump = database.ref('posts').orderByChild('index');
+    var dataDump = database.ref('posts').orderByChild('index');
 
     dataDump.on('value', function(snapshot) {
         snapshot.forEach(function(childSnapshot) {
             var childData = childSnapshot.val();
             var postNum = childData['index'];
-            string_of_posts += '<h3><li><a id="titleLink" href="#" onclick="displayPost(' + postNum + ');return false;">' + childData['title'] + '</a></li></h3>';                  
-            // Display all tags of a post
-            string_of_posts +=  '<h5><strong>Tags </strong>';
-            for(var i = 0; i < childData['tags'].length; i++) {
-                string_of_posts += '<mark>&nbsp;' + childData['tags'][i] + '&nbsp;</mark>&nbsp;&nbsp;';
-            }
+            // check if tags match - if they do, add to HTML string
+            
+            string_of_posts += '<h3><li><a id="titleLink" href="#" onclick="displayPost(' + postNum + ');return false;">' + childData['title'] + '</a></li></h3>';
+
             string_of_posts += '</h5>';
         });
-        string_of_posts += '</ul><br />things!</div>';
         // Map HTML to #main div content
         var list_of_posts = string_of_posts;
-        $('#posts').html(list_of_posts);   
+        $('#posts').html(list_of_posts);
     });
 }
 
@@ -68,8 +55,54 @@ function updateFilters() {
 }
 
 function toggleTag(num) {
-    console.log("toggle taggy");
-    var taggy = '.tagNum' + num;
-    console.log("taggy: " + taggy);
+    var taggy = '#tagNum' + num;
+    var curr = $(taggy).css("background-color");
+    console.log(curr);
+    if(curr=="rgb(255, 255, 255)") {
+        $(taggy).css("background-color", "var(--post-two-color)");
+    } else {
     $(taggy).css("background-color", "white");
+  }
+  showTaggedPosts();
+}
+
+function showNoTags() {
+  var tags = $("#tags").children("mark");
+  for(var i = 0; i < tags.length; i++) {
+    $(tags[i]).css("background-color", "white");
+  }
+  showTaggedPosts();
+}
+
+function showAllTags() {
+  console.log("turning on all tags...");
+  var tags = $("#tags").children("mark");
+  for(var i = 0; i < tags.length; i++) {
+    $(tags[i]).css("background-color", "var(--post-two-color)");
+  }
+  showTaggedPosts();
+}
+
+function getTagList() {
+  // return a list of all tags displayed on the page
+  var tags = $("#tags").children("mark");
+  tagList = new Set();
+  for(var i = 0; i < tags.length; i++) {
+    tagList.add($.trim($(tags[i]).text()));
+  }
+  return tagList;
+}
+
+function getSelectedTagList() {
+  // return a list of all user selected tags
+  var tags = $("#tags").children("mark");
+  tagList = new Set();
+  for(var i = 0; i < tags.length; i++) {
+    var curr = $(tags[i]).css("background-color");
+    if(curr != "rgb(255, 255, 255)") {
+      tagList.add($.trim($(tags[i]).text()));
+  }
+  }
+  return tagList;
+
 }
